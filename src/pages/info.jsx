3e052b6fcf43cf50de17f90999b9982e6467e29a
Ryan Yeo <ryandc.yeo@gmail.com>
2024-06-06 02:08:45 -0700

    json

---
+++ src/pages/info.jsx
@@ -0,0 +1,18 @@
+// import PropTypes from 'prop-types'; 
+import './info.css'; 
+
+export default function Info() {
+    return (
+        <div className='info-page'>
+            <h1>pH</h1>
+            <p>text 1</p>
+            <p>text 2</p>
+            <button>Back</button>
+        </div>
+    )
+}
+
+// Info.propTypes = {
+//     title: PropTypes.string.isRequired,
+//     isRequired: PropTypes.bool
+// }
